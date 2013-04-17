333c9b226fa0fc5ab86fb9cd94d86056efea634b
blueblank <blueblank@gmail.com>
2013-04-17 19:08:03 -0400

    Fleem is exportable

--- flask_fleem/__init__.py
+++ flask_fleem/__init__.py
@@ -1,4 +1,4 @@
 __version__ = '0.0.2'
 from theme import Theme
 from theme_manager import ThemeManager
-from fleem import get_themes_list, render_theme_template
+from fleem import Fleem, active_theme, get_themes_list, render_theme_template
